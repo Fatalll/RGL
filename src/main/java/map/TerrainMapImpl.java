eacd0499a0b26da9169570c0e02b085a7da57758
akhoroshev <arthoroshev@gmail.com>
2019-04-06 18:45:46 +0300

    Add impl

---
+++ src/main/java/map/TerrainMapImpl.java
@@ -0,0 +1,42 @@
+package map;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class TerrainMapImpl implements TerrainMap {
+    private TerrainCellType [][] terrain;
+    private Point enterPoint;
+    private Point exitPoint;
+    private int height;
+    private int width;
+
+    public TerrainMapImpl(TerrainCellType [][] terrain, Point enterPoint, Point exitPoint, int width, int height) {
+        this.terrain = terrain;
+        this.enterPoint = enterPoint;
+        this.exitPoint = exitPoint;
+        this.height = height;
+        this.width = width;
+    }
+
+    @Override
+    public @NotNull Point getDimensions() {
+        return new Point(width, height);
+    }
+
+    @Override
+    public @NotNull Point getEnterPoint() {
+        return enterPoint;
+    }
+
+    @Override
+    public @NotNull Point getExitPoint() {
+        return exitPoint;
+    }
+
+    @Override
+    public @NotNull TerrainCellType getCellType(Point position) {
+        return terrain[position.x][position.y];
+    }
+}
